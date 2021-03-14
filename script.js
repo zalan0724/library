66787d06ebd2bf5586a65f822f4a5977f55eaced
zalan0724 <munkacsi0724@gmail.com>
2021-03-14 21:02:20 +0100

    scripting

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function displayNewBookInteraface(){
-    
-}
+let addButton = document.querySelector(".addButton");
+let newBookInterface = document.querySelector('.newBookInterface');
+let newBookCancel = document.querySelector('#newBookCancel');
+
+
+
+
+
+
+
+
+addButton.addEventListener('click',()=>{
+    newBookInterface.style.display = 'flex';
+})
+newBookCancel.addEventListener('click', ()=>{
+    newBookInterface.style.display = 'none';
+})
